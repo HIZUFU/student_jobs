a24308d131c7f08f5dcf2619c69d8cb9d478387e
Андрей Дианов <a.d200333@gmail.com>
2026-04-01 21:39:41 +0500

    feat: add POST endpoint for creating vacancies

--- app/routes/vacancy.py
+++ app/routes/vacancy.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models.vacancy import Vacancy
+from app.extensions import db
 from app.utils.localization import localize_vacancy
 
 vacancy_bp = Blueprint("vacancy", __name__)
@@ -7,7 +8,23 @@ vacancy_bp = Blueprint("vacancy", __name__)
 @vacancy_bp.route("/vacancies", methods=["GET"])
 def get_vacancies():
     vacancies = Vacancy.query.all()
-
     result = [localize_vacancy(v) for v in vacancies]
-
     return jsonify(result)
+
+
+@vacancy_bp.route("/vacancies", methods=["POST"])
+def create_vacancy():
+    data = request.get_json()
+
+    new_vacancy = Vacancy(
+        title_en=data.get("title_en"),
+        title_de=data.get("title_de"),
+        description_en=data.get("description_en"),
+        description_de=data.get("description_de"),
+        company=data.get("company")
+    )
+
+    db.session.add(new_vacancy)
+    db.session.commit()
+
+    return jsonify({"message": "Vacancy created"}), 201
