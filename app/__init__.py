852167d50cede5c3b136a8128d8d648d1d52aad7
Андрей Дианов <a.d200333@gmail.com>
2026-04-01 21:14:39 +0500

    fix: create and connect Postgresql database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from .config import Config
+from .extensions import db
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
 
     @app.route("/")
     def home():
-        return "App works!"
+        return "DB connected"
 
     return app
